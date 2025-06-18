8e1bb0454940bd5cfdeac89a3e8d209ec2d457a2
Jung Young Hak <jungyh870918@gmail.com>
2025-06-18 18:34:04 +0900

    임시 메가 네브 추가

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,6 +4,7 @@ import { APP_NAME } from '@/lib/constants';
 import Menu from './menu';
 import CategoryDrawer from './category-drawer';
 import Search from './search';
+import MegaMenu from './MegaMenu';
 
 const Header = () => {
   return (
@@ -29,9 +30,10 @@ const Header = () => {
         <div className='hidden md:block w-[300px]'>
           <Search />
         </div>
-
+        
         <Menu />
       </div>
+      <div><MegaMenu /></div>
     </header>
   );
 };
