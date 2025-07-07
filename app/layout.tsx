50d3c7f5f68247e3742c504e7d00378902181154
jungyh870918 <jungyh870918@gmail.com>
2025-07-07 22:51:40 +0900

    db 환경변수 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // This is the root layout for the application.
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={`${inter.className} antialiased`}>
